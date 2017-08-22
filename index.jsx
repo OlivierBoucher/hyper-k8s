1d240b8f1077d044a93347d333d8a1a5df285775
Olivier Boucher <boucher.oli@gmail.com>
2017-08-22 12:36:53 -0400

    completed plugin

--- index.jsx
+++ index.jsx
@@ -1,26 +1,84 @@
 /**
  * Created by olivier on 2016-12-13.
  */
+const fs = require('fs');
+const watch = require('node-watch');
+const yaml = require('js-yaml');
+
+const ActionK8sContextChanged = "K8S_CONTEXT_CHANGED"
 
 exports.onApp = (app) => {
     console.log(app)
 };
 
-exports.decorateTerm = (Term, {React, notify}) => {
+exports.onWindow = (BrowserWindow) => {
+    const kubeConfigPath = '/Users/olivier/.kube/config'
+
+    const getKubeConfig = () => yaml.safeLoad(fs.readFileSync(kubeConfigPath, 'utf8'));
+    const emitConfig = () => {
+        const config = getKubeConfig();
+        const context = config.contexts.find(x => x.name === config['current-context']).context
+        const clusterSplit = context.cluster.split('_');
+
+        BrowserWindow.rpc.emit("KUBE_STATE_CHANGE", {
+            namespace: context.namespace || 'default',
+            cluster: clusterSplit[clusterSplit.length -1],
+        })
+    }
+
+    watch(kubeConfigPath, {}, (event, file) => {
+        emitConfig();
+    })
+    setTimeout(() => {
+        emitConfig();
+    }, 2000);
+}
+
+exports.decorateTerm = (Term, { React, notify }) => {
     const BottomBar = require('./src/components/BottomBar')(React);
 
     return class extends React.Component {
         constructor(props, context) {
             super(props, context)
+
+            window.rpc.on('KUBE_STATE_CHANGE', (kubeState) => {
+                window.store.dispatch({
+                    type: ActionK8sContextChanged,
+                    value: kubeState,
+                });
+            });
         }
 
-        render () {
+        render() {
             return React.createElement(Term, Object.assign({}, this.props, {
                 padding: '12px 14px 32px 14px',
                 customChildren: [
-                    <BottomBar key="wazz"/>
+                    <BottomBar key="bottomBar" kubeState={this.props.kubeState} />
                 ]
             }));
         }
     }
 };
+
+exports.reduceUI = (state, action) => {
+    switch (action.type) {
+        case ActionK8sContextChanged:
+            return state.set('kubeState', action.value);
+    }
+    return state;
+}
+
+exports.mapTermsState = (state, map) => {
+    return Object.assign(map, {
+        kubeState: state.ui.kubeState,
+    })
+}
+
+const passProps = (uid, parentProps, props) => {
+    return Object.assign(props, {
+        kubeState: parentProps.kubeState,
+    })
+}
+
+exports.getTermGroupProps = passProps;
+exports.getTermProps = passProps;
--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,16 +1,42 @@
 module.exports = (React) => {
+	const KubernetesLogo = require('./KubernetesLogo')(React);
+
 	return class BottomBar extends React.Component {
 		constructor(props) {
 			super(props)
+
+			window.rpc.on('KUBE_STATE_CHANGE', (state) => {
+				console.log(state);
+			});
 		}
 
 		render() {
+			const Status = ({ kubeState }) => {
+				if (kubeState) {
+					return <div style={{ marginLeft: 5 }}>
+						<span>{kubeState.cluster}</span>
+						<span style={{ marginLeft: 5, marginRight: 5 }}>{'\u2192'}</span>
+						<span>{kubeState.namespace}</span>
+					</div>
+				} else {
+					return <span style={{ marginLeft: 5 }}>Loading...</span>
+				}
+			}
 			return <div style={{
-				height: 33,
+				fontSize: '12px',
+				height: 35,
 				paddingLeft: 10,
 				paddingRight: 10,
+				paddingBottom: 5,
 				marginLeft: -15,
-				marginRight: -15}}>Wazzaa</div>
+				marginRight: -15,
+				display: 'flex',
+				flexDirection: 'row',
+				alignItems: 'center'
+			}}>
+				<KubernetesLogo></KubernetesLogo>
+				<Status kubeState={this.props.kubeState}></Status>
+			</div>
 		}
 	}
 };
